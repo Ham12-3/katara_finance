00ab5100bee21cec3ba4e3360bf557fd44d56ab8
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-10-05 17:19:05 +0100

    update

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -2,9 +2,46 @@ import { Box, useTheme } from "@mui/material";
 
 type Props = {};
 
+const gridTemplate = `
+"a b c"
+"a b c"
+"a b c"
+"a b f"
+"d e f"
+"d e f"
+"d h i"
+"g h i"
+"g h j"
+"g h j"
+`;
+
 const Dashboard = (props: Props) => {
   const { palette } = useTheme();
-  return <Box color={palette.grey[300]}></Box>;
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={{
+        gridTemplateColumns: "repeat(3, miman(370px, 1fr)",
+        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+        gridTemplateAreas: gridTemplate,
+      }}
+    >
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="a"></Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
